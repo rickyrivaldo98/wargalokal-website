27980f4a0a974b9847be262a4857b61e760f58ba
rickyrivaldo98 <rickyrivaldo98@gmail.com>
2021-08-21 20:43:33 +0700

    tab

--- src/pages/VolunteerDesa.tsx
+++ src/pages/VolunteerDesa.tsx
@@ -3,8 +3,17 @@ import TabPerkembangan from "src/components/volunteer/tabPerkembangan"
 import Footer from "src/Layout/footer";
 import Navbar from "src/Layout/navbar";
 import headerBg from "../assets/bg.png";
+import { useState } from 'react';
+import * as React from 'react';
 
-const VolunteerDesa = () => {
+const VolunteerDesa: React.FC = () => {
+    const [activeTab, setActiveTab] = useState("tab1");
+    const handleTab1 = () => {
+        setActiveTab("tab1")
+    };
+    const handleTab2 = () => {
+        setActiveTab("tab2")
+    }
     return (
         <>
             <Navbar />
@@ -42,8 +51,29 @@ const VolunteerDesa = () => {
                     </div>
                 </div>
                 <div className="bg-white mt-20">
-                    <TabDeskripsi />
-                    <TabPerkembangan />
+                    <div className="mx-auto w-11/12">
+                        <div className="mt-6 bg-gray-100 rounded-md">
+                            <ul className="flex">
+                                <li className={"py-4 px-8 mx-12 " + (activeTab === "tab1" ? "text-green-500 border-b-4 border-green-500" : "text-gray-400")} onClick={handleTab1}>Deskripsi</li>
+                                <li className={"py-4 px-8 mx-12 " + (activeTab === "tab2" ? "text-green-500 border-b-4 border-green-500" : "text-gray-400")} onClick={handleTab2}>Perkembangan</li>
+                                <li className="text-gray-400 py-4 px-8 mx-12">FAQ</li>
+                                <li className="text-gray-400 py-4 px-8 mx-12">Komentar</li>
+                                <li className="text-gray-400 py-4 px-8 mx-12">Galeri</li>
+                            </ul>
+                        </div>
+                        {
+                            activeTab === "tab1" ?
+                                <div className="bg-gray-100 mt-10">
+                                    <TabDeskripsi />
+                                </div>
+                                : activeTab === "tab2" ?
+                                    <div className="bg-gray-100 mt-10">
+                                        <TabPerkembangan />
+                                    </div> :
+                                    <div>OTW</div>
+                        }
+
+                    </div>
                 </div>
             </div>
             <Footer />
