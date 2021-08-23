1a52a6b29ec4b55ff1bf0d9d75823cce27456261
rickyrivaldo98 <rickyrivaldo98@gmail.com>
2021-08-23 12:41:52 +0700

    FAQ

--- src/pages/VolunteerDesa.tsx
+++ src/pages/VolunteerDesa.tsx
@@ -4,15 +4,19 @@ import Footer from "src/Layout/footer";
 import Navbar from "src/Layout/navbar";
 import headerBg from "../assets/bg.png";
 import { useState } from 'react';
-import * as React from 'react';
+import TabFAQ from "src/components/volunteer/tabFAQ";
 
-const VolunteerDesa: React.FC = () => {
+
+const VolunteerDesa = () => {
     const [activeTab, setActiveTab] = useState("tab1");
     const handleTab1 = () => {
         setActiveTab("tab1")
     };
     const handleTab2 = () => {
         setActiveTab("tab2")
+    }
+    const handleTab3 = () => {
+        setActiveTab("tab3")
     }
 
     return (
@@ -42,9 +46,9 @@ const VolunteerDesa: React.FC = () => {
                         <div className="flex text-center w-full">
                             <div className="w-2/6 py-3">
                                 <div>5 Perusahaan Sudah Apply</div>
-                                <div className="mt-2 py-2 bg-primary text-white rounded-full">Peraturan syarat dan ketentuan disini</div>
+                                <div className="mt-2 py-2 px-2 bg-primary text-white rounded-full">Peraturan syarat dan ketentuan disini</div>
                             </div>
-                            <div className="w-3/6 py-6 px-3">
+                            <div className="w-3/6 py-6 px-3 self-center">
                                 <div className="py-3 bg-primary text-white rounded-full">Apply!</div>
                             </div>
                             <div className="w-1/6"></div>
@@ -57,7 +61,7 @@ const VolunteerDesa: React.FC = () => {
                             <ul className="flex">
                                 <li className={"py-4 px-8 mx-12 " + (activeTab === "tab1" ? "text-green-500 border-b-4 border-green-500" : "text-gray-400")} onClick={handleTab1}>Deskripsi</li>
                                 <li className={"py-4 px-8 mx-12 " + (activeTab === "tab2" ? "text-green-500 border-b-4 border-green-500" : "text-gray-400")} onClick={handleTab2}>Perkembangan</li>
-                                <li className="text-gray-400 py-4 px-8 mx-12">FAQ</li>
+                                <li className={"py-4 px-8 mx-12 " + (activeTab === "tab3" ? "text-green-500 border-b-4 border-green-500" : "text-gray-400")} onClick={handleTab3}>FAQ</li>
                                 <li className="text-gray-400 py-4 px-8 mx-12">Komentar</li>
                                 <li className="text-gray-400 py-4 px-8 mx-12">Galeri</li>
                             </ul>
@@ -71,6 +75,10 @@ const VolunteerDesa: React.FC = () => {
                                 'tab2':
                                     <div className="bg-gray-100 mt-10">
                                         <TabPerkembangan />
+                                    </div>,
+                                'tab3':
+                                    <div className="bg-gray-100 mt-10">
+                                        <TabFAQ />
                                     </div>
                             }[activeTab]
                         }
