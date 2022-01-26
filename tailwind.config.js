d18bce0ab3c7ae692256a94c2c3fe1ff3dedc207
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2022-01-26 21:34:05 +0700

    fix: tailwind purge path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    // purge: [],
-    purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    purge: ["./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         backgroundColor: (theme) => ({
