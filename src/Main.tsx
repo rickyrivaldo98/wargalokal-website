6bf48476bcaa49112f178074353337a90b633850
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2022-02-14 20:21:03 +0700

    Revert "serverside"
    This reverts commit cbf715ca84d7c98d5d55c9d4bc327557669dd965.

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.hydrate(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("app")
+  document.getElementById("root")
 );
